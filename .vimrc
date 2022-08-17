1a749af9c808213b449385c2160bcb8362ca1c82
Santiago Vrancovich <103597690+santiagovrancovich@users.noreply.github.com>
2022-08-17 20:04:29 -0300

    Initial commit

---
+++ .vimrc
@@ -0,0 +1,7 @@
+set bg=dark
+syntax on
+set number
+color gruvbox
+let g:gruvbox_contrast_dark='hard'
+let g:airline#extensions#tabline#enabled = 1
+let g:airline_theme='gruvbox'
---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+shopt -s extglob
+
+# Plugins to install
+plugins=(
+  morhetz/gruvbox
+  vim-airline/vim-airline
+)
+
+# Remove any old plugins
+rm -rf $HOME/.vim/pack
+mkdir -p $HOME/.vim/pack/bundle/start
+
+# Move .vimrc config file to home directory
+cp -i .vimrc ~/.vimrc
+
+installplugin() {
+  # This removes the GitHub username and two common Vim plugin name
+  # prefix/sufixes
+  local plugin="$(echo "$1" | sed -e 's/.*[\/]//' -e 's/^vim-//' -e 's/\.vim//')"
+  local pluginpath="$HOME/.vim/pack/bundle/start/$plugin"
+
+  # Clone without the plugin's history (--depth=1) to make download faster
+  git clone --depth=1 -q https://github.com/$1.git "$pluginpath"
+
+  # Remove git-related files to prevent nested git repositories
+  rm -rf "$pluginpath"/.git*
+
+  # Remove any non-essential files that are not needed for the plugin to run
+  rm -rf "$pluginpath"/README*
+  rm -f "$pluginpath"/*.@(md|mdown|mkdown|markdown)
+  rm -rf "$pluginpath"/test
+
+  # Print a line announcing installation of the plugin on top of any previous announcements.
+  echo "Installed $plugin"
+}
+
+# The & is so that bash sets the installation as a background job, allowing for
+# asynchronous installation (all plugins being installed at the same time).
+for repo in ${plugins[@]}; do
+  installplugin "$repo" &
+done
+
+# This makes the script wait for all the background jobs from installing all the
+# plugins above before continuing
+wait
